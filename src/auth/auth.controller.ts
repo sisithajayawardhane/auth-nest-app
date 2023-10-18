e1fcfc1d0ab359a419a9fa75449618a62221d787
sisithajayawardhane <sisithajayawardhane@gmail.com>
2023-10-18 22:28:56 +0530

    created auth-controller

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { LoginDto } from './dto/login';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('login')
+  login(@Body() loginDto: LoginDto) {
+    return this.authService.login(loginDto);
+  }
+
+  @Post('register')
+  register(@Body() loginDto: LoginDto) {
+    return this.authService.register(loginDto);
+  }
+}
