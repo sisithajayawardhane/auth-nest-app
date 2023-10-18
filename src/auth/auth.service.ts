9ad7718eb561177a025022584a0023104cff6031
sisithajayawardhane <sisithajayawardhane@gmail.com>
2023-10-18 22:23:12 +0530

    created auth-service

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,50 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User } from './schemas/user-schema';
+import * as bcrypt from 'bcrypt';
+import { LoginDto } from './dto/login';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(@InjectModel(User.name) private usrModel: Model<User>) {}
+
+  async login(loginDto: LoginDto) {
+    const user = await this.usrModel.findOne({
+      username: loginDto.username,
+    });
+
+    if (!user) {
+      return 'Invalid User Name';
+    }
+
+    const isMatch = await bcrypt.compare(loginDto.password, user.password);
+
+    if (!isMatch) {
+      return 'Invalid Password';
+    }
+
+    return 'Login successful';
+  }
+
+  async register(loginDto: LoginDto) {
+    const { password, username } = loginDto;
+    const user = await this.usrModel.findOne({
+      username: loginDto.username,
+    });
+
+    if (user) {
+      return 'User already exists';
+    }
+
+    const saltOrRounds = 10;
+    const hash = await bcrypt.hash(password, saltOrRounds);
+
+    await this.usrModel.create({
+      username,
+      password: hash,
+    });
+
+    return 'User created';
+  }
+}
