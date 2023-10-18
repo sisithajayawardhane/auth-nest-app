a5d97fd5d6885189c78375ab560807d64ab70ba1
sisithajayawardhane <sisithajayawardhane@gmail.com>
2023-10-18 22:10:41 +0530

    connected mongodb

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [AuthModule],
+  imports: [MongooseModule.forRoot('mongodb://localhost/mongodb'), AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
